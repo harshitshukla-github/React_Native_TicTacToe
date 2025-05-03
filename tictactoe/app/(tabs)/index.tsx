7c98e70c3b77c73f92a7f15b9c62a5a1000149c4
HARSHIT SHUKLA <32368309+harshitshukla-github@users.noreply.github.com>
2025-05-03 18:24:32 +0000

    Build a game of Tic-Tac-Toe with expo features enabled.

---
+++ tictactoe/app/(tabs)/index.tsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity, StatusBar, Alert } from "react-native";
+
+export default function TicTacToe(){
+  <View>
+    <StatusBar barStyle="dark-content"/>
+  </View>
+}
